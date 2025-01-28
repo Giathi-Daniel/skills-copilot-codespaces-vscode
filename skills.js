13b9e2b8fc1100cfd6f7429c5539cba8d3ee8bfc
Daniel Giathi <127479628+Giathi-Daniel@users.noreply.github.com>
2025-01-28 10:35:32 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,19 @@
+function calculateNumbers(var1, var2) {
+    console.log(var1 + var2);
+}
+
+function subtractNumbers(var1, var2) {
+    console.log(var1 - var2);
+}
+
+function multiplyNumbers(var1, var2) {
+    console.log(var1 * var2);
+}
+
+function divideNumbers(var1, var2) {
+    if (var2 !== 0) {
+        console.log(var1 / var2);
+    } else {
+        console.log("Cannot divide by zero");
+    }
+}
